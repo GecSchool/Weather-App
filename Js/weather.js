a23a12fae7cc54d9cf0b72d001d8386b516d9285
GecSchool <grw7407@naver.com>
2022-07-20 04:54:25 +0900

    main screen complete without img

--- Js/weather.js
+++ Js/weather.js
@@ -1,60 +1,82 @@
-let weatherApi = {
-    API_KEY: "91cce404db7f7f79c1684fc4b4022752",
-    lat : 0,
-    lon : 0,
+const weatherApi = {
+  API_KEY: "91cce404db7f7f79c1684fc4b4022752",
+  lat: 0,
+  lon : 0,
 }
 
 const geoNowloca = function geoNowloca() {
-  if (!navigator.geolocation){
-   console.log("Geolocation is not supported by your browser");
-    return;
-  }
-  function success(position) {
-    weatherApi.lat  = position.coords.latitude;
-    weatherApi.lon = position.coords.longitude;
-  }
-  function error() {
-    console.log("Unable to retrieve your location");
-  }
-  navigator.geolocation.getCurrentPosition(success, error);
-
+  return new Promise(function(resolve){
+    if (!navigator.geolocation){
+     console.log("Geolocation is not supported by your browser");
+      return;
+    }
+    function success(position) {
+      weatherApi.lat  = position.coords.latitude;
+      weatherApi.lon = position.coords.longitude;
+      resolve();
+    }
+    function error() {
+      console.log("Unable to retrieve your location");
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
+  })
 }
+
+const getWeather = function getWeather(){
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${weatherApi.lat}&lon=${weatherApi.lon}&appid=${weatherApi.API_KEY}`
+  fetch(url)
+  .then((response) => response.json())
+  .then((data)=>{
+    console.log(data);
+    console.log(data.city.country);
+    mainScreen(data);
+  })
+}
+
 const targetLocation = function targetLocation(){
   const savedloca = JSON.parse(localStorage.getItem('location'));
   if(savedloca){
     // Yes
     weatherApi.lat = savedloca.lat;
     weatherApi.lon = savedloca.lon;    
+    getWeather();
   } else{
     // No get a new location
-    geoNowloca();
-    
+    geoNowloca().then(getWeather);
   }
-  getWeather();
 }
-const getWeather = function getWeather(){
-  const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${weatherApi.lat}&lon=${weatherApi.lon}&appid=${weatherApi.API_KEY}`
-  fetch(url)
-    .then((response) => response.json())
-    .then((data)=>{
-      console.log(data);
-    })
+function mainScreen(data){
+  const date = new Date();
+  const day = document.querySelector(".day");
+  const todayDate = document.querySelector(".date");
+  const location  = document.querySelector(".location");
+  const weatherSummery = document.querySelector(".today_weather__summary__weather");
+  const temperature = document.querySelector(".today_weather__summary__temperature");
+  const screen = document.querySelector(".today_weather");
+  day.textContent = date.toLocaleString('en-US',{weekday:'long'});
+  todayDate.textContent = `${String(date.getDay()).padStart(2,"0")} ${date.toLocaleString('en-US',{month:'short'})} ${date.getFullYear()}`;
+  location.textContent = data.city.name;
+  const iconcode = data.list[0].weather[0].icon
+  const url = `http://openweathermap.org/img/w/${iconcode}.png`
+  $('#wicon').attr('src', url);
+  const weather = data.list[0].weather[0].main
+  weatherSummery.textContent = weather;
+  temperature.textContent = `${Math.round(data.list[0].main.temp-273.15,2)}℃`;
+  screen.style.backgroundImage = `url(../img/${weather}Nights.jpg)`;
 }
-// function onGeoOk(position){
-//     const lat = position.coords.latitude;
-//     const lon = position.coords.longitude;
-//     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-//     fetch(url)
-//         .then((response) => response.json())
-//         .then((data)=>{
-//             const weather = document.querySelector("#weather p:last-child");
-//             const city = document.querySelector("#weather p:first-child");
-//             city.innerText = data.name;
-//             weather.innerText = `${data.weather[0].main} ${Math.round(data.main.temp)}℃`;
-//         });
+function subScreen(){
 
+}
+targetLocation();
+
+// const clock = document.querySelector("#clock");
+// const getClock = function getClock(){
+//     const date = new Date();
+//     const hours = String(date.getHours()).padStart(2,"0");
+//     const minutes = String(date.getMinutes()).padStart(2,"0");
+//     const seconds = String(date.getSeconds()).padStart(2,"0");
+//     clock.innerText = `${hours}:${minutes}:${seconds}`
 // }
-// function onGeoError(){
-//     console.log("Can't find you");
-// }
-// navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
+
+// getClock();
+// setInterval(getClock,1000);
